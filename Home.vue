71d2c67f9b6e912f81fbad222e28b05a917a9064
lifechoices <mkhosanasiyabonga@gmail.com>
2022-02-21 09:09:10 +0200

    new portfolio push

---
+++ Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home">
+    <Landing/>
+  </div>
+</template>
+
+<script>
+import Landing from '../components/landing.vue'
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+/* Home page */
+.home{ 
+  background-image: url('../assets/Images/Landing/Office.jpg');
+  background-size: cover;
+  height: 100vh;
+
+}
+.Home .slide{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-sizing: border-box;
+}
+/* Typewriter animation inspired by CSS Tricks*/
+.headings h1 {
+    overflow: hidden; 
+    border-right: .15em solid #F5A281;
+    white-space: nowrap;
+    margin: 0 auto; 
+    letter-spacing: .15em; 
+    animation: 
+      typing 3.5s steps(40, end),
+      blink-caret .75s step-end infinite;
+      animation-iteration-count: 1;
+  }
+  /* The typing effect */
+  @keyframes typing {
+    from { width: 0 }
+    to { width: 60% }
+  }
+  /* The headings cursor effect */
+  @keyframes blink-caret {
+    from, to { border-color: transparent }
+    50% { border-color: black; }
+}
+/* My social widgets */
+.social-widget-header{
+    position: absolute;
+    margin: 20px 30px 0;
+    padding: 15px 0;
+    justify-content: center;
+}
+</style>
---
+++ Resume.vue
@@ -0,0 +1,291 @@
+<template>
+  <section class="single-section alt-bg CV-area" id="CV-area">
+    <div class="container">
+      <div class="row text-center justify-content-center mb-5">
+        <div class="col-xl-6 col-lg-8">
+          <h1
+            class="text-uppercase font-weight-bolder"
+            style="
+              box-shadow: inset 0 -6px 0 #f5a281;
+              color: white;
+              margin-bottom: 30px;
+            "
+          >
+            Resume
+          </h1>
+          <h5 class="p-tag1">
+            I am very proud of the path I have taken. Explore the history that
+            made me the professional that I am today.
+          </h5>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <div class="timeline-steps aos-init aos-animate" data-aos="fade-up">
+            <div class="timeline-step">
+              <div
+                class="timeline-content"
+                data-toggle="popover"
+                data-trigger="hover"
+                data-placement="top"
+                title="Matriculated with a Bachelor's degree"
+                data-content="I started high school in 2010 and finished in 2013."
+                data-original-title="2010"
+              >
+                <div class="inner-circle"></div>
+                <div data-aos="zoom-in"></div>
+                <p class="h6 mt-3 mb-1">2010-2013</p>
+                <p class="p-tag mb-0 mb-lg-0">Peak View Secondary School</p>
+              </div>
+            </div>
+            <div class="timeline-step">
+              <div
+                class="timeline-content"
+                data-toggle="popover"
+                data-trigger="hover"
+                data-placement="top"
+                title="My first job and through the Youth Leadership programme of the University of the Western Cape, in partnership with the City of Cape Town."
+                data-content="A jolly good experience helping communities, it didn't even feel like a job."
+                data-original-title="2004"
+              >
+                <div class="inner-circle"></div>
+                <p class="h6 mt-3 mb-1">July 2014-Jan 2015</p>
+                <p class="p-tag mb-0 mb-lg-0">Sonke Gender Justice</p>
+              </div>
+            </div>
+            <div class="timeline-step">
+              <div
+                class="timeline-content"
+                data-toggle="popover"
+                data-trigger="hover"
+                data-placement="top"
+                title="The work culture of this company is fantastic, the professionalism as well. I learnt a lot working as a Debt Counselor, that is where I learnt much communication skills."
+                data-content="That was the beginning of my financial independence."
+                data-original-title="2005"
+              >
+                <div class="inner-circle"></div>
+                <p class="h6 mt-3 mb-1">March 2018-Sep 2018</p>
+                <p class="p-tag mb-0 mb-lg-0">Meerkat Debt Counselors</p>
+              </div>
+            </div>
+            <div class="timeline-step">
+              <div
+                class="timeline-content" 
+                data-toggle="popover"
+                data-trigger="hover"
+                data-placement="top"
+                title="A1 Brokers helped me unlock my potential in the workspace."
+                data-content="And here's some amazing content. It's very engaging. Right?"
+                data-original-title="2010"
+              >
+                <div class="inner-circle"></div>
+                <p class="h6 mt-3 mb-1">May 2021-Sep 2021</p>
+                <p class="p-tag mb-0 mb-lg-0">A1 Financial Brokers</p>
+              </div>
+            </div>
+            <div class="timeline-step mb-0">
+              <div
+                class="timeline-content"
+                data-toggle="popover"
+                data-trigger="hover"
+                data-placement="top"
+                title="Life Choices Coding Academy is opening wider doors for young web developers, I am proud to be a part of this journey."
+                data-content="Web Development"
+                data-original-title="2020"
+              >
+                <div class="inner-circle"></div>
+                <p class="h6 mt-3 mb-1">Sep 2021-</p>
+                <p class="p-tag mb-0 mb-lg-0">Life Choice Coding Academy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="Resume" id="Resume">
+    <div data-aos="flip-left"
+    data-aos-easing="ease-out-cubic"
+    data-aos-duration="2000">  
+    <div class="container">        
+    <div class="row text-center justify-content-center mb-5">
+        <div class="col-xl-6 col-lg-8">
+            <h2 class="font-weight-bold">Resume</h2>
+            <p class="text-muted">Explore my history.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="timeline-steps aos-init aos-animate" data-aos="fade-up">
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2013">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">2010-2013</p>
+                        <h3>Peak View Secondary School</h3>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2014">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">July 2014-Jan 2015</p>
+                        <h3>Sonke Gender Justice</h3>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2018">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">March 2018-Sep 2018</p>
+                        <h3>Meerkat Debt Councellors</h3>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2021">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">May 2021-Sep 2021</p>
+                        <h3>A1 Financial Brokers</h3>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2021">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">Sep 2021-</p>
+                        <h3>Life Choice Coding Academy</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div> -->
+    <div
+      class="bts text-uppercase"
+      style="text-align: center; margin-top: 50px"
+    >
+      <a
+        href="https://resume-example.com/builder/resume/02c62779-dcaa-41f7-8009-9ee21b644039/preview"
+        target="blank"
+        class="btn extra"
+        style="border-color: blue; background-color: #f5a281; font-weight: bold"
+        >Download Resume</a
+      >
+    </div>
+    <!-- </div> -->
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Resumes: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:5000/Resumes")
+      .then((response) => {
+        console.log("hey");
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.Resumes = data;
+      });
+  },
+};
+</script>
+
+<style>
+.container {
+  margin-top: 150px;
+}
+
+body {
+  margin-top: 20px;
+}
+
+.timeline-steps {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.timeline-steps .timeline-step {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 1rem;
+}
+
+.font-weight-bold {
+  margin-top: 60px;
+}
+
+@media (min-width: 768px) {
+  .timeline-steps .timeline-step:not(:last-child):after {
+    content: "";
+    display: block;
+    border-top: 0.25rem dotted #3b82f6;
+    width: 3.46rem;
+    position: absolute;
+    left: 7.5rem;
+    top: 0.3125rem;
+  }
+  .timeline-steps .timeline-step:not(:first-child):before {
+    content: "";
+    display: block;
+    border-top: 0.25rem dotted #3b82f6;
+    width: 3.8125rem;
+    position: absolute;
+    right: 7.5rem;
+    top: 0.3125rem;
+  }
+}
+
+.timeline-steps .timeline-content {
+  width: 10rem;
+  text-align: center;
+}
+
+.timeline-steps .timeline-content .inner-circle {
+  border-radius: 1.5rem;
+  height: 1rem;
+  width: 1rem;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #3b82f6;
+}
+
+.timeline-steps .timeline-content .inner-circle:before {
+  content: "";
+  background-color: #3b82f6;
+  display: inline-block;
+  height: 3rem;
+  width: 3rem;
+  min-width: 3rem;
+  border-radius: 6.25rem;
+  opacity: 0.5;
+}
+.Resume {
+  height: 100vh;
+  width: cover;
+  padding: 60px 0 40px;
+  z-index: 9478470375037507094755085370953;
+}
+.p-tag1 {
+  color: white;
+  font-weight: bolder;
+}
+.p-tag {
+  color: white;
+  font-weight: bold;
+}
+.single-section {
+  background-color: gray;
+  height: 100vh;
+  overflow-x: auto;
+}
+</style>
